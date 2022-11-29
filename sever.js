60374ba65013f8afa7500b3e7579b6e092dee149
Kenshin1609 <nobitabadao788@gmail.com>
2022-11-29 15:57:09 +0700

    admin

--- sever.js
+++ sever.js
@@ -3,12 +3,22 @@ const PORT = 8000;
 const url = require('url');
 const OrderController = require("./controller/home.controller");
 const orderController=new OrderController()
+const adminController = require('./controller/Admin.Home')
+const userController = new adminController()
 const server=http.createServer((req, res)=>{
     let urlPath = url.parse(req.url);
     switch (urlPath.pathname) {
         case '/homePage':
             orderController.index(req,res);
             break;
+        case '/admin':
+            userController.index(req,res);
+            break;
+        case '/delete':
+            console.log(1)
+            userController.deleteUser(req,res);
+            break;
+
     }
 
 
